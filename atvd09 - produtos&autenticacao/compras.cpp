cc7c3502b4b6361ed9c56e404140a50c34faa3e0
arthalm <arthurbr912@gmail.com>
2026-05-07 22:17:38 -0300

    continuação de compras pt3

--- atvd09 - produtos&autenticacao/compras.cpp
+++ atvd09 - produtos&autenticacao/compras.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Produto
@@ -89,112 +90,87 @@ public:
 
 int main()
 {
-    int operacao_ini;
-    int operacao_sec;
-    int tamanho = 1;
-    bool terminou;
-    Produto *dados;
-    Produto *lista_exibicao = new Produto[tamanho];
+    int operacao;
+    vector<Produto *> lista_exibicao;
 
-    cout << "SUPERMERCADO LYMPIA\nO que deseja realizar?\n1 - Cadastrar novo produto\n2 - Exibir lista de produtos cadastrados\n0 - Sair\nSua escolha: ";
-    cin >> operacao_ini;
+    cout << "SUPERMERCADO LYMPIA\nO que deseja realizar?\n1 - Cadastrar produto de limpeza\n2 - Cadastrar produto alimentício\n3 - Cadastrar produto eletrônico\n4 - Exibir lista de produtos cadastrados\n0 - Sair\nSua escolha: ";
+    cin >> operacao;
 
-    while (operacao_ini != 0 && terminou == false)
+    while (operacao != 0)
     {
-        if (operacao_ini == 1)
+        if (operacao == 1)
         {
-            while (operacao_sec != 4 && terminou == false)
-            {
-                cout << "Qual tipo de produto você deseja cadastrar?\n1 - Produto de limpeza\n2 - Produto alimentício\n3 - Produto eletrônico\n4 - Sair\nSua escolha: ";
-                cin >> operacao_sec;
-                if (operacao_sec == 1)
-                {
-                    ProdutoLimpeza *pl = new ProdutoLimpeza();
-                    string nm, tipo;
-                    double pr;
-                    cout << "Digite o nome do produto: ";
-                    cin >> nm;
-                    pl->setNome(nm);
-                    cout << "Digite o preço do produto em R$ (use '.' para simbolizar a vírgula): ";
-                    cin >> pr;
-                    pl->setPreco(pr);
-                    cout << "Digite o tipo do produto (opções disponíveis: Doméstico, Hospitalar, Industrial): ";
-                    cin >> tipo;
-                    pl->setTipo(tipo);
-                    lista_exibicao[tamanho - 1] = *pl;
-                    tamanho++;
-                    cout << "Encerrando cadastro de produto de limpeza...\n";
-                }
-                else if (operacao_sec == 2)
-                {
-                    ProdutoAlimenticio *pa = new ProdutoAlimenticio();
-                    string nm, val;
-                    double pr;
-                    cout << "Digite o nome do produto: ";
-                    cin >> nm;
-                    cout << "Digite o preço do produto em R$ (use '.' para simbolizar a vírgula): ";
-                    cin >> pr;
-                    cout << "Digite a data de validade do produto (use o formato DD/MM/AAAA com as barras inclusas): ";
-                    cin >> val;
-                    pa->setPreco(pr);
-                    pa->setNome(nm);
-                    pa->setValidade(val);
-                    lista_exibicao[tamanho - 1] = *pa;
-                    tamanho++;
-                    cout << "Encerrando cadastro de produto alimentício...\n";
-                }
-                else if (operacao_sec == 3)
-                {
-                    ProdutoEletronico *pe = new ProdutoEletronico();
-                    int gar, volt;
-                    string nm;
-                    double pr;
-                    cout << "Digite o nome do produto: ";
-                    cin >> nm;
-                    pe->setNome(nm);
-                    cout << "Digite o preço do produto em R$ (use '.' para simbolizar a vírgula): ";
-                    cin >> pr;
-                    pe->setPreco(pr);
-                    cout << "Digite a voltagem do produto em volts: ";
-                    cin >> volt;
-                    pe->setVoltagem(volt);
-                    cout << "Digite a garantia do produto em anos :";
-                    cin >> gar;
-                    pe->setGarantia(gar);
-                    lista_exibicao[tamanho - 1] = *pe;
-                    tamanho++;
-                    cout << "Encerrando cadastro de produto eletrônico...\n";
-                }
-                else if (operacao_sec == 4)
-                {
-                    cout << "Saindo do cadastro de produtos...\n";
-                    terminou = true;
-                }
-                else
-                {
-                    throw invalid_argument("Erro! A operação digitada não está disponível. Tente novamente.");
-                }
-            }
+            ProdutoLimpeza *pl = new ProdutoLimpeza();
+            string nm, tipo;
+            double pr;
+            cout << "Digite o nome do produto: ";
+            cin >> nm;
+            pl->setNome(nm);
+            cout << "Digite o preço do produto em R$ (use '.' para simbolizar a vírgula): ";
+            cin >> pr;
+            pl->setPreco(pr);
+            cout << "Digite o tipo do produto (opções disponíveis: Doméstico, Hospitalar, Industrial): ";
+            cin >> tipo;
+            pl->setTipo(tipo);
+            lista_exibicao.push_back(pl);
+            cout << "Encerrando cadastro de produto de limpeza...\n";
         }
-        else if (operacao_ini == 2)
+        else if (operacao == 2)
+        {
+            ProdutoAlimenticio *pa = new ProdutoAlimenticio();
+            string nm, val;
+            double pr;
+            cout << "Digite o nome do produto: ";
+            cin >> nm;
+            cout << "Digite o preço do produto em R$ (use '.' para simbolizar a vírgula): ";
+            cin >> pr;
+            cout << "Digite a data de validade do produto (use o formato DD/MM/AAAA com as barras inclusas): ";
+            cin >> val;
+            pa->setPreco(pr);
+            pa->setNome(nm);
+            pa->setValidade(val);
+            lista_exibicao.push_back(pa);
+            cout << "Encerrando cadastro de produto alimentício...\n";
+        }
+        else if (operacao == 3)
+        {
+            ProdutoEletronico *pe = new ProdutoEletronico();
+            int gar, volt;
+            string nm;
+            double pr;
+            cout << "Digite o nome do produto: ";
+            cin >> nm;
+            pe->setNome(nm);
+            cout << "Digite o preço do produto em R$ (use '.' para simbolizar a vírgula): ";
+            cin >> pr;
+            pe->setPreco(pr);
+            cout << "Digite a voltagem do produto em volts: ";
+            cin >> volt;
+            pe->setVoltagem(volt);
+            cout << "Digite a garantia do produto em anos :";
+            cin >> gar;
+            pe->setGarantia(gar);
+            lista_exibicao.push_back(pe);
+            cout << "Encerrando cadastro de produto eletrônico...\n";
+        }
+        else if (operacao == 4)
         {
             cout << "Exibindo a lista de produtos cadastrados: \n";
-            for (int i = 0; i < tamanho; i++)
+            for (Produto *p : lista_exibicao)
             {
-                Produto p = dados[i];
-                p.exibirInformacoes();
+                p->exibirInformacoes();
+                cout << endl;
             }
         }
-        else if (operacao_ini == 0)
+        else if (operacao == 0)
         {
             cout << "Saindo..." << endl;
-            terminou = true;
         }
         else
         {
             throw invalid_argument("Erro! A operação digitada não está disponível. Tente novamente.");
         }
-        cout << "SUPERMERCADO LYMPIA\nO que deseja realizar?\n1 - Cadastrar novo produto\n2 - Exibir lista de produtos cadastrados\n0 - Sair\nSua escolha: ";
-        cin >> operacao_ini;
+        cout << "SUPERMERCADO LYMPIA\nO que deseja realizar?\n1 - Cadastrar produto de limpeza\n2 - Cadastrar produto alimentício\n3 - Cadastrar produto eletrônico\n4 - Exibir lista de produtos cadastrados\n0 - Sair\nSua escolha: ";
+        cin >> operacao;
     }
 }

2d9532473f932d06c3afc64e5bf258a68f40e4f2
arthalm <arthurbr912@gmail.com>
2026-05-14 20:02:43 -0300

    continuação da atividade do jogo

--- atvd09 - produtos&autenticacao/autenticacao.cpp
+++ atvd09 - produtos&autenticacao/autenticacao.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 class Autenticacao
 {
-
 public:
     virtual void coletarDados() = 0;
 
--- atvd10 - jogo/jogo.cpp
+++ atvd10 - jogo/jogo.cpp
@@ -1,38 +1,222 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Personagem
 {
-    double health_points;
-    int mana_points = 100;
     double attack;
+    double current_health;
+    double max_health;
+    int current_mana;
+    int max_mana;
 
 public:
-    virtual void setHp(double hp)
+    Personagem(double atk, double hp, int mn) : attack(atk), max_health(hp), current_health(hp), max_mana(mn), current_mana(mn) {}
+    virtual ~Personagem() {}
+    double getAttack()
     {
-        health_points = hp;
+        return attack;
+    }
+    double getCurrentHealth()
+    {
+        return current_health;
+    }
+    double getMaxHealth()
+    {
+        return max_health;
+    }
+    int getCurrentMana()
+    {
+        return current_mana;
+    }
+    int getMaxMana()
+    {
+        return max_mana;
     }
 
-    virtual void setAttack(double at)
+    virtual double ataqueSimples() = 0;
+    virtual double ataqueCarregado() = 0;
+
+    void passagemTurno()
     {
-        attack = at;
+        if (current_health < max_health)
+        {
+            current_health += max_health * 0.1;
+            if (current_health > max_health)
+            {
+                current_health = max_health;
+            }
+        }
+
+        if (current_mana < max_mana)
+        {
+            current_mana += max_mana * 0.1;
+            if (current_mana > max_mana)
+            {
+                current_mana = max_mana;
+            }
+        }
+    }
+
+    bool usarMana()
+    {
+        if (current_mana >= max_mana * 0.15)
+        {
+            current_mana -= max_mana * 0.15;
+            return true;
+        }
+        else
+        {
+            cout << "Mana abaixo de 15%! Espere a rodada acabar." << endl;
+            return false;
+        }
+    }
+
+    void tomarDano(double dmg)
+    {
+        current_health -= dmg;
+    }
+
+    bool estaVivo()
+    {
+        if (current_health > 0.00)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 };
 
 class Mago : public Personagem
 {
+public:
+    Mago() : Personagem(15.00, 300.00, 250) {}
 
-    public:
+    double ataqueSimples() override
+    {
+        cout << "O Mago ataca com a sua magia!" << endl;
+        return getAttack();
+    }
 
+    double ataqueCarregado() override
+    {
+        if (usarMana() == true)
+        {
+            cout << "O Mago usa Magia Suprema!" << endl;
+            getAttack() * 2;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 };
 
 class Guerreiro : public Personagem
 {
+public:
+    Guerreiro() : Personagem(20.00, 500.00, 100) {}
+
+    double ataqueSimples() override
+    {
+        cout << "O Guerreiro ataca com a sua espada!" << endl;
+        return getAttack();
+    }
+
+    double ataqueCarregado() override
+    {
+        if (usarMana() == true)
+        {
+            cout << "O Guerreiro usa Corte Dimensional!" << endl;
+            getAttack() * 2;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 };
 
 class Arqueiro : public Personagem
 {
+public:
+    Arqueiro() : Personagem(25.00, 150.00, 150) {}
+
+    double ataqueSimples() override
+    {
+        cout << "O Arqueiro atira uma flecha!" << endl;
+        return getAttack();
+    }
+
+    double ataqueCarregado() override
+    {
+        if (usarMana() == true)
+        {
+            cout << "O Arqueiro usa Flecha Perfurante!" << endl;
+            getAttack() * 2;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 };
+
+void acaoPersonagem(Personagem *p, vector<Personagem *> &players, int opcao)
+{
+    vector<string> classes{"Mago", "Guerreiro", "Arqueiro"};
+    cout << "---- Atacante: " << classes[opcao] << " ----" << endl;
+    cout << "HP atual: " << p->getCurrentHealth() << " - Mana atual: " << p->getCurrentMana() << endl;
+
+    cout << "Quem você deseja atacar?" << endl;
+    vector<Personagem *> adversarios;
+
+    int incrementador = 1;
+    for (int i = 0; i < players.size(); i++)
+    {
+        if (players[i] != p && players[i]->estaVivo())
+        {
+            cout << "Opções de quem atacar" << endl;
+            cout << incrementador << " - " << classes[i] << " - HP atual: " << players[i]->getCurrentHealth() << " - Mana atual: " << players[i]->getCurrentMana() << endl;
+            adversarios.push_back(players[i]);
+            incrementador++;
+        }
+    }
+
+    int atk_adv;
+    cin >> atk_adv;
+    do
+    {
+        cerr << "Erro! o número digitado não corresponde a nenhum dos personagens disponíveis. Tente novamente." << endl;
+        cin >> atk_adv;
+    } while (atk_adv < 1 || atk_adv > adversarios.size());
+    Personagem *adversario = adversarios[atk_adv - 1];
+
+    cout << "Qual tipo de ataque deseja realizar?\n1 - Ataque Normal\n2 - Ataque Carregado\nSua escolha: ";
+    int esc_atk;
+    cin >> esc_atk;
+    do
+    {
+        cerr << "Erro! o número digitado não corresponde a nenhum dos ataques disponíveis. Tente novamente." << endl;
+        cin >> esc_atk;
+    } while (esc_atk < 1 || esc_atk > 2);
+
+    double dmg = 0.00;
+    if (esc_atk == 1)
+    {
+        dmg = p->ataqueSimples();
+    }
+    else
+    {
+        dmg = p->ataqueCarregado();
+    }
+    adversario->tomarDano(dmg);
+
+    cout << classes[opcao] << " deu " << dmg << " pontos de dano ao " << adversario[atk_adv]
+}
 
 int main()
 {
